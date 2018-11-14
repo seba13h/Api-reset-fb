da84c85b7fbf708dcd96cf665108c039858058ea
sebastian <sebastian@analitic.cl>
2018-11-14 09:40:46 -0300

    add server

--- extractor/fbapirest/server.js
+++ extractor/fbapirest/server.js
@@ -2,10 +2,13 @@ var express = require("express"),
     app = express(),
     bodyParser  = require("body-parser"),
     methodOverride = require("method-override");
+    mongoose = require('mongoose');
 
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
-app.use(methodOverride());
+app.configure(function () {
+  app.use(express.bodyParser());
+  app.use(express.methodOverride());
+  app.use(app.router);
+});
 
 var router = express.Router();
 
@@ -13,7 +16,13 @@ router.get('/', function(req, res) {
    res.send("Hello World!");
 });
 
-app.use(router);
+mongoose.connect('mongodb://localhost/tvshows', function(err, res) {
+	if(err) {
+		console.log('ERROR: connecting to Database. ' + err);
+	} else {
+		console.log('Connected to Database');
+	}
+});
 
 app.listen(3000, function() {
   console.log("Node server running on http://localhost:3000");
