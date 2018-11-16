7ba9fd3b229e52ea182e889e8f8d6d78dd55ae3c
sebastian <sebastian@analitic.cl>
2018-11-16 13:48:35 -0300

    add ends point fp and tokens

--- extractor/fbapirest/code/app.js
+++ extractor/fbapirest/code/app.js
@@ -7,7 +7,7 @@ var bodyParser  = require("body-parser");
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-
+//end points fan pages
 app.post('/controller/addFanpage', function (req, res) {
   console.log(req.body);
   res.status(200).send({ message: 'se recibe la fp'});
@@ -22,7 +22,43 @@ app.get('/controller/listAllFanpage', function (req, res){
 		res.send(200, {fanpages: reply});
 	})
 });
+app.get('/controller/listFanpageError', function (req, res){
+	redisClient.smembers('fanpagesError',function(err, reply){
+		res.send(200, {fanpages con error: reply});
+	})
+});
+app.get('/controller/listFanpageNoError', function (req, res){
+	redisClient.smembers('fanpagesNoError',function(err, reply){
+		res.send(200, {fanpages sin error: reply});
+	})
+});
+app.get('/controller/listFanpageSinValidar', function (req, res){
+	redisClient.smembers('fanpagesSinValidar',function(err, reply){
+		res.send(200, {fanpages sin validar: reply});
+	})
+});
+//end point tokens
+app.post('/controller/addToken', function (req, res) {
+  console.log(req.body);
+  res.status(200).send({ message: 'se recibe token'});
+  let key = req.body.key;
+  let token = req.body.name;
+  redisClient.sadd(key, token);
+  console.log('token guardado');	
+});
 
+app.get('/controller/listToken', function (req, res){
+	redisClient.smembers('tokens',function(err, reply){
+		res.send(200, {tokens: reply});
+	})
+});
+
+app.get('/controller/listTokenErroneos', function (req, res){
+	redisClient.smembers('tokensErroneos',function(err, reply){
+		res.send(200, {tokens con error: reply});
+	})
+});
+//delete
 app.delete('/controller/deleteFanpage/:idFp', function (req, res){
 	let key = req.params.idFp;
 	console.log(key);
